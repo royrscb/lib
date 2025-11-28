bf46c5d3c70588ef7073b417c85ba50b99dea9a7
gombit.9310 <royrscb@gmail.com>
2025-11-28 11:33:36 +0100

    per fi to va PERFECTE

--- tsx/EditableInputFactory.tsx
+++ tsx/EditableInputFactory.tsx
@@ -7,9 +7,10 @@ export interface BaseEditableInputProps<T> {
     className?: string;
     style?: React.CSSProperties;
     button: EditableInputButtonProps;
-    value: T | null;
+    initialValue: T | null;
     // eslint-disable-next-line no-unused-vars
     onSave: (value: T | null) => Promise<boolean | void> | boolean | void;
+    inputName?: string;
     inputTitle?: React.ReactNode;
     showEditor?: boolean;
     nullable?: boolean;
@@ -21,19 +22,22 @@ export interface EditableInputButtonProps {
     content: React.ReactNode;
 }
 
+// Inputs ---------------------------------------
+
 export function TextInput(props: BaseEditableInputProps<string> & {
     placeHolder?: string;
 }): React.JSX.Element {
     return <EditableInputWrapper<string>
         {...props}
-        renderInput={(value, setValue) =>
+        renderInput={(text, setText) =>
             <input
+                name={props.inputName ?? 'editableText'}
                 required={props.requireValue}
                 autoFocus
-                className='w-100 fs-20 mt-3'
+                className='w-100'
                 type='text'
-                value={value ?? ''}
-                onChange={e => setValue(e.currentTarget.value)}
+                value={text ?? ''}
+                onChange={e => setText(e.currentTarget.value)}
                 placeholder={props.placeHolder}
             />
         }
@@ -46,13 +50,14 @@ export function TextAreaInput(props: BaseEditableInputProps<string> & {
 }): React.JSX.Element {
     return <EditableInputWrapper<string>
         {...props}
-        renderInput={(value, setValue) =>
+        renderInput={(text, setText) =>
             <textarea
+                name={props.inputName ?? 'editableTextarea'}
                 required={props.requireValue}
                 autoFocus
-                className='w-100 fs-20 mt-3'
-                value={value ?? ''}
-                onChange={e => setValue(e.currentTarget.value)}
+                className='w-100'
+                value={text ?? ''}
+                onChange={e => setText(e.currentTarget.value)}
                 rows={props.rows ?? 3}
                 placeholder={props.placeHolder}
             />
@@ -64,91 +69,29 @@ export function DateInput(props: BaseEditableInputProps<Date> & {
     min?: Date;
     max?: Date;
 }): React.JSX.Element {
+    function getValidInputDateValueOrEmpty(date: Date | undefined | null): string {
+        return date && !isNaN(date.getTime())
+            ? date.toInputDateValue() : '';
+    }
+
     return <EditableInputWrapper<Date>
         {...props}
-        renderInput={(value, setValue) =>
+        renderInput={(date, setDate) =>
             <input
-                className='w-100 fs-20 mt-3'
+                name={props.inputName ?? 'editableDate'}
+                required={props.requireValue}
+                className='w-100'
                 type='date'
-                value={value?.toInputDateValue()}
-                min={props.min?.toInputDateValue()}
-                max={props.max?.toInputDateValue()}
-                onChange={e => setValue(new Date(e.currentTarget.value))}
+                value={getValidInputDateValueOrEmpty(date)}
+                min={getValidInputDateValueOrEmpty(props.min)}
+                max={getValidInputDateValueOrEmpty(props.max)}
+                onChange={e => setDate(new Date(e.currentTarget.value))}
             />
         }
     />;
 }
 
-function EditableInputWrapper<T>(props: BaseEditableInputProps<T> & {
-    // eslint-disable-next-line no-unused-vars
-    renderInput: (value: T | null, setValue: React.Dispatch<React.SetStateAction<T | null>>) => React.ReactNode;
-}): React.JSX.Element {
-    const formRef = React.useRef<HTMLFormElement | null>(null);
-
-    const [showEditor, setShowEditor] = React.useState<boolean>(
-        props.showEditor || !!props.requireValue && !props.value
-    );
-    const [value, setValue] = React.useState<T | null>(
-        !props.nullable && props.value === null ? ('' as T) : props.value
-    );
-    const showForm = value !== null || !props.nullable || props.requireValue;
-
-    async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-
-        const res = await props.onSave(value);
-        if (res !== false) {
-            setShowEditor(false);
-        }
-    }
-    function onClose() {
-        setShowEditor(false);
-        setValue(props.value);
-    }
-
-    React.useEffect(() => console.log(value), [value]);
-
-    return <div style={props.style} className={clsx('editable-input', props.className)}>
-        {!showEditor
-            ? <PreviewButton {...props.button} showInput={() => setShowEditor(true)}/>
-            : <div className='bg-color-lightgray p-5 br-10'>
-                {props.inputTitle}
-                <form hidden={!showForm} ref={formRef} onSubmit={onSubmit}>
-                    {props.renderInput(value, setValue)}
-                </form>
-                <SetNullInput<T> {...props} value={value} setValue={setValue} />
-                <CloseSaveButtons
-                    onClose={props.requireValue && !props.value ? undefined : onClose}
-                    formRef={formRef}
-                />
-            </div>
-        }
-    </div>;
-}
-
-function SetNullInput<T>(props: BaseEditableInputProps<T> & {
-    value: T | null;
-    setValue: React.Dispatch<React.SetStateAction<T | null>>;
-}): React.JSX.Element | undefined {
-    if (!props.nullable || props.requireValue)
-        return;
-
-    const setNullInputId = `setNull-${generateUuid()}`;
-
-    return <div className='mt-3 mb-3'>
-        <input
-            id={setNullInputId}
-            type='checkbox'
-            className='mr-5'
-            checked={props.value === null}
-            onChange={e => {
-                const checked = e.currentTarget.checked;
-                props.setValue(checked ? null : props.value ?? ('' as T));
-            }}
-        />
-        <label htmlFor={setNullInputId} className='fs-smaller-2 no-select'>null</label>
-    </div>;
-}
+// Private --------------------------------------
 
 function PreviewButton(props: EditableInputButtonProps & {
     showInput: () => void;
@@ -162,11 +105,89 @@ function PreviewButton(props: EditableInputButtonProps & {
     </button>;
 }
 
+function EditableInputWrapper<T>(props: BaseEditableInputProps<T> & {
+    // eslint-disable-next-line no-unused-vars
+    renderInput: (value: T | null, setValue: React.Dispatch<React.SetStateAction<T | null>>) => React.ReactNode;
+}): React.JSX.Element {
+    const formRef = React.useRef<HTMLFormElement | null>(null);
+
+    const needUrgentValue = !props.initialValue && !!props.requireValue;
+    const [showEditor, setShowEditor] = React.useState<boolean>(props.showEditor || needUrgentValue);
+    const [value, setValue] = React.useState<T | null>(props.initialValue);
+    const [isNullChecked, setIsNullChecked] = React.useState<boolean>(props.initialValue === null);
+
+    async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const res = await props.onSave(value);
+        if (res !== false) {
+            setShowEditor(false);
+        }
+    }
+    function onClose() {
+        setShowEditor(false);
+        setValue(props.initialValue);
+    }
+
+    return <div style={props.style} className={clsx('editable-input', props.className)}>
+        {!showEditor
+            ? <PreviewButton {...props.button} showInput={() => setShowEditor(true)}/>
+            : <div className='bg-color-lightgray p-5 br-10'>
+                {props.inputTitle}
+                <form
+                    hidden={isNullChecked && !props.requireValue && props.nullable}
+                    ref={formRef}
+                    onSubmit={onSubmit}
+                    className='mt-3'
+                >
+                    {props.renderInput(value, setValue)}
+                </form>
+                <SetNullInput<T>
+                    {...props}
+                    isChecked={isNullChecked} setIsChecked={setIsNullChecked}
+                    setValue={setValue}
+                />
+                <CloseSaveButtons
+                    onClose={needUrgentValue ? undefined : onClose}
+                    onSave={() => formRef.current!.requestSubmit()}
+                />
+            </div>
+        }
+    </div>;
+}
+
+function SetNullInput<T>(props: BaseEditableInputProps<T> & {
+    isChecked: boolean;
+    setIsChecked: React.Dispatch<React.SetStateAction<boolean>>;
+    setValue: React.Dispatch<React.SetStateAction<T | null>>;
+}): React.JSX.Element | undefined {
+    if (props.requireValue || !props.nullable)
+        return;
+
+    const setNullInputId = `setNull-${generateUuid()}`;
+
+    return <div className='mt-3 mb-3'>
+        <input
+            id={setNullInputId}
+            type='checkbox'
+            className='mr-5'
+            checked={props.isChecked}
+            onChange={e => {
+                const checked = e.currentTarget.checked;
+
+                props.setIsChecked(checked);
+                props.setValue(checked ? null : props.initialValue);
+            }}
+        />
+        <label htmlFor={setNullInputId} className='fs-smaller-2 no-select'>null</label>
+    </div>;
+}
+
 function CloseSaveButtons(props: {
     onClose?: () => void;
-    formRef: React.RefObject<HTMLFormElement | null>;
+    onSave: () => void;
 }): React.JSX.Element {
-    return <div className='d-flex mt-5'>
+    return <div className='close-save-buttons d-flex mt-5'>
         <button
             disabled={!props.onClose}
             onClick={props.onClose}
@@ -176,7 +197,7 @@ function CloseSaveButtons(props: {
             <icons.Cross color='white'/>
         </button>
         <button
-            onClick={() => props.formRef.current?.requestSubmit()}
+            onClick={props.onSave}
             type='submit'
             className='flex-center center-v w-50 ml-3 bg-color-limegreen'
             style={{padding: '2px 0', marginBottom: 0}}
